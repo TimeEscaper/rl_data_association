1034c33bc4c2a6f76bd1dcf53022444c068cbdc6
User <MacBook@Users-MacBook-Air-2.local>
2020-03-29 12:25:34 +0800

    added da ic method

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# rl_data_association
-Data Association in SLAM using Reinforcement Learning ("Perception in Robotics" Skoltech course project)
+# Data association. Individual compatibility method (mahalanobis) 
+
+Added ic.py file with IC class
+
+## Running Code
+
+Parameters (same as in PS3)
+
+To get association you need use method .get_association(observation, state) and it will return list with found indexes
---
+++ data_ass_individual_compatibility/ic.py
@@ -0,0 +1,51 @@
+import numpy as np
+from tools.task import *
+from tools.jacobian import *
+from scipy.stats import chi2
+
+
+def get_observation_(state, landmark):
+    dx = landmark[0] - state[0]
+    dy = landmark[1] - state[1]
+
+    distance = np.sqrt(dx ** 2 + dy ** 2)
+    bearing = np.arctan2(dy, dx) - state[2]
+
+    return np.array([distance, wrap_angle(bearing)])
+
+
+class IC:
+    def __init__(self, field_map, beta):
+        self.lm_map = [list(a) for a in zip(list(field_map.landmarks_poses_x), list(field_map.landmarks_poses_y))]
+        beta[1] = np.deg2rad(beta[1])
+        self.Q = np.diag([*(beta ** 2)])
+
+    def get_association(self, observation, state):
+        output_indexes = []
+
+        for z in observation:
+            obs_lm_index = None
+
+            for lm in self.lm_map:
+                innovation = z[:2] - get_observation_(state, lm)
+                H = observation_jacobian(state, lm)[1]
+                P = self.get_landmark_covariance_(state, get_observation_(state, lm))
+                cov = H @ P @ H.T + self.Q
+                mah_dis = innovation @ np.linalg.inv(cov) @ innovation.T
+
+                if mah_dis < chi2.isf(0.05, 2):
+                    obs_lm_index = self.lm_map.index(lm)
+
+            output_indexes.append(obs_lm_index)
+
+        return output_indexes
+
+    def get_landmark_covariance_(self, state, observation):
+        W = inverse_observation_jacobian(state, observation)[1]
+        return W @ self.Q @ W.T
+
+    def consruct_hypothethis(self, z, state):
+        H = []
+        for lm in self.lm_map:
+            H.append(z - get_observation_(state, lm))
+        return H
